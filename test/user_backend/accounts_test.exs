8e4d151e3ae212eb70cc801c2ade31f2a37d127a
hiltpold <m.hiltpold@protonmail.ch>
2021-08-08 11:53:23 +0200

    fixed tests

--- test/user_backend/accounts_test.exs
+++ test/user_backend/accounts_test.exs
@@ -1,17 +1,18 @@
-defmodule UserBackend.AccountTest do
+defmodule UserBackend.AccountsTest do
   use UserBackend.DataCase
-  alias UserBackend.Account
-  alias UserBackend.Account.Notification
+  alias UserBackend.Accounts
+  alias UserBackend.Accounts.Notification
+  alias UserBackend.Accounts.User
   use ExUnit.Case
   require Logger
   use Bamboo.Test
 
   describe "users" do
-    alias UserBackend.Account.User
-    @valid_attrs %{email: "some@mail.com", password: "password", role: "user"}
+    alias UserBackend.Accounts.User
+    @valid_attrs %{email: "some@mail.com", password: "Password#1", role: "user"}
     @update_attrs %{
-      email: "updated@mail.com",
-      password: "some updated password"
+      email: "some_other@mail.com",
+      password: "Password#2"
     }
     @invalid_attrs %{email: nil, password: nil, role: nil, }
 
@@ -19,58 +20,58 @@ defmodule UserBackend.AccountTest do
       {:ok, user} =
         attrs
         |> Enum.into(@valid_attrs)
-        |> Account.create_user()
+        |> Accounts.create_user()
       user
     end
 
     test "list_users/0 returns all users" do
       user = user_fixture()
-      assert Account.list_users() == [%{user | password: nil}]
+      assert Accounts.list_users() == [%{user | password: nil}]
     end
 
     test "get_user!/1 returns the user with given id" do
       user = user_fixture()
-      assert Account.get_user!(user.id) == %{user | password: nil}
+      assert Accounts.get_user!(user.id) == %{user | password: nil}
     end
 
     test "create_user/1 with valid data creates a user" do
-      assert {:ok, %User{} = user} = Account.create_user(@valid_attrs)
+      assert {:ok, %User{} = user} = Accounts.create_user(@valid_attrs)
       assert user.email == "some@mail.com"
       assert user.is_verified == false
       assert user.role == "user"
-      assert Bcrypt.verify_pass("password", user.password_hash)
+      assert Bcrypt.verify_pass("Password#1", user.password_hash)
    end
 
    test "create_user/1 with invalid data returns error changeset" do
-     assert {:error, %Ecto.Changeset{}} = Account.create_user(@invalid_attrs)
+     assert {:error, %Ecto.Changeset{}} = Accounts.create_user(@invalid_attrs)
    end
 
   test "update_user/2 with valid data updates the user" do
     user = user_fixture()
-    assert {:ok, %User{} = user} = Account.update_user(user, @update_attrs)
-    assert {:ok, %User{} = user} = Account.update_user_password(user.email, @update_attrs)
-    assert user.email == "updated@mail.com"
+    assert {:ok, %User{} = user} = Accounts.update_user(user, @update_attrs)
+    assert {:ok, %User{} = user} = Accounts.update_user_password(user.email, @update_attrs)
+    assert user.email == "some_other@mail.com"
     assert user.is_verified  == false
-    assert Bcrypt.verify_pass("some updated password", user.password_hash)
+    assert Bcrypt.verify_pass("Password#2", user.password_hash)
   end
 
   test "update_user/2 with invalid data returns error changeset" do
     user = user_fixture()
-    test =Account.update_user(user, @invalid_attrs)
+    test =Accounts.update_user(user, @invalid_attrs)
     Logger.info(">>>>>>>>>#{inspect(test)}")
-    assert {:error, %Ecto.Changeset{}} = Account.update_user(user, @invalid_attrs)
-    assert %{user | password: nil} == Account.get_user!(user.id)
+    assert {:error, %Ecto.Changeset{}} = Accounts.update_user(user, @invalid_attrs)
+    assert %{user | password: nil} == Accounts.get_user!(user.id)
   end
 
   test "delete_user/1 deletes the user" do
     user = user_fixture()
-    assert {:ok, %User{}} = Account.delete_user(user)
-    assert_raise Ecto.NoResultsError, fn -> Account.get_user!(user.id) end
+    assert {:ok, %User{}} = Accounts.delete_user(user)
+    assert_raise Ecto.NoResultsError, fn -> Accounts.get_user!(user.id) end
   end
 
   test "change_user/1 returns a user changeset" do
     user = user_fixture()
-    assert %Ecto.Changeset{} = Account.change_user(user)
+    assert %Ecto.Changeset{} = Accounts.change_user(user)
   end
 
    test "deliver_confirmation_instructions/2 delivers account confirmation mail" do
